47a20c82a529d748e8f7ac71e8c683b59b4fde75
Patrício Samuel <patricioanjosjf@gmail.com>
2023-07-24 17:48:15 -0300

    correção da animação

--- sript.js
+++ sript.js
@@ -6,3 +6,14 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('bg-primary')
     }
 })
+
+//carrega a animação depois que a imagem de fundo for carregada
+window.onload = function () {
+    var backgroundImg = new Image()
+    backgroundImg.src = "img/background-azul.jpg"
+    backgroundImg.onload = function () {
+        var bannerSection = document.getElementById('my-banner')
+        bannerSection.style.backgroundImage = "url('img/background-azul.jpg')"
+        AOS.init()
+    };
+};
